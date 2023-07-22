bf2ab5ee49c1e5b264e5138bd4dd1983b099a23b
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2023-07-22 01:53:13 +0100

    implemented append method

---
+++ linkedlist.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Linkedlist:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def printList(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+
+
+my_linked_list = Linkedlist(4)
+
